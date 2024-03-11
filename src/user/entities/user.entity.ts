fded4c3a9823c6e6dcd880528eee6450af2c4bca
유민식 <soule125@naver.com>
2024-03-11 16:52:54 +0900

    user 엔티티 작성

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "../userRole.type";
+@Entity({
+  name: "user"
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column({ type: "varchar", length: 13, nullable: false })
+  name: string;
+
+  @Column({ type: "int", length: 3, nullable: false })
+  age: number;
+
+  @Column({ type: "varchar", length: 2, nullable: false })
+  gender: string;
+
+  @Column({ type: "varchar", length: 50, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: "varchar", select: false, length: 13, nullable: false })
+  password: string;
+
+  @Column({ type: "varchar", length: 13, nullable: true })
+  phoneNumber: string;
+
+  @Column({ type: "enum", enum: Role, default: Role.User })
+  @CreateDateColumn()
+  createdAt: Date;
+}
