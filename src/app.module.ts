34523722948ac9a63d3550ca97a957007c77df12
유민식 <soule125@naver.com>
2024-03-11 16:03:01 +0900

    nest g resouce 했음

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConcertModule } from './concert/concert.module';
+import { ReservationModule } from './reservation/reservation.module';
+import { PointModule } from './point/point.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ConcertModule, ReservationModule, PointModule],
   controllers: [AppController],
   providers: [AppService],
 })
