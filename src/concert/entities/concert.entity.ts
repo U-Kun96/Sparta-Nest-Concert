039741ef5128475d3a3c1e2ec99b381de3bd179a
유민식 <soule125@naver.com>
2024-03-11 17:26:23 +0900

    콘서트 엔티티 작성

--- src/concert/entities/concert.entity.ts
+++ src/concert/entities/concert.entity.ts
@@ -1,1 +1,27 @@
-export class Concert {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+  name: "concert"
+})
+export class Concert {
+  @PrimaryGeneratedColumn()
+  concertId: number;
+
+  @Column({ type: "varchar", length: 50, nullable: false })
+  title: string;
+
+  @Column({ type: "varchar", length: 20, nullable: false })
+  content: string;
+
+  @Column({ type: "varchar", length: 20, nullable: false })
+  artist: string;
+
+  @Column({ type: "int", length: 7, nullable: false })
+  price: number;
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  location: string;
+
+  @Column({ type: "varchar", length: 30, nullable: false })
+  concertDay: string;
+}
